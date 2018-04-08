b841291a0e87b62bb551b2c43ea4fc8f1fb988a8
Suman19291 <sumananandan@hotmail.com>
2018-04-08 11:42:48 +0530

    removed sh

--- vars/StandardPipeline.groovy
+++ vars/StandardPipeline.groovy
@@ -14,23 +14,23 @@ def call(body) {
                     checkout scm
                 }
                 stage ('Build') {
-                    sh "echo 'building ${config.projectName} ...'"
+                     
                     bat "nuget restore ${config.projectName}.sln"
 		            bat "\"${tool 'MSBuild'}\" ${config.projectName}.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
                 }
                 stage ('Tests') {
                     parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
+                        
                     },
                     'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
+                        
                     },
                     'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
+                        
                     }
                 }
                 stage ('Deploy') {
-                    sh "echo 'deploying to server ${config.serverDomain}...'"
+                    
                 }
             } catch (err) {
                 currentBuild.result = 'FAILED'
