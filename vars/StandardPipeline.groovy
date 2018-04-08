86b23a3c4ff81cc729cd04cc5915aa49521f2e1e
Suman19291 <sumananandan@hotmail.com>
2018-04-08 13:25:36 +0530

    Nuget removed

--- vars/StandardPipeline.groovy
+++ vars/StandardPipeline.groovy
@@ -14,8 +14,6 @@ def call(body) {
                     checkout scm
                 }
                 stage ('Build') {
-                     
-                    nuget restore "${config.projectName}.sln"
 		            bat "\"${tool 'MSBuild'}\" ${config.projectName}.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
                 }
                 stage ('Tests') {
